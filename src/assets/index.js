abcda7385942a567a4dfdd10482a8a1c9ecf90d9
Nick3353 <nicholas3353@gmail.com>
2024-04-02 00:22:04 +0100

    added a ReusableCarousel component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,31 @@
-import Bars from './icons/Bars.svg'
-import logo from './images/logo.svg'
-import food from './images/food.svg'
-import hero_img from './images/hero_img.png'
-import hero_imge from './images/hero_imge.png'
-export
-{
-    Bars,
-    logo,
-    food,
-    hero_img,
-    hero_imge,
-}
+
+import Bars from "./icons/Bars.svg";
+import logo from "./images/logo.svg";
+import food from "./images/food.svg";
+import hero_img from "./images/hero_img.png";
+import hero_imge from "./images/hero_imge.png";
+import sneaker from "./images/sneaker.png";
+import men_clothes from "./images/men_clothes.png";
+import ladies_dress from "./images/ladies_dress.png";
+import win_kitchen from "./images/win_kitchen.png";
+import stores from "./images/stores.png"
+import casio_watch from './images/casio_watch.png'
+import iphone12 from  './images/iphone12.png'
+import brown_jacket from './images/brown_jacket.png'
+import hd_monitor from './images/hd_monitor.png'
+export {
+  Bars,
+  logo,
+  food,
+  hero_img,
+  hero_imge,
+  sneaker,
+  men_clothes,
+  ladies_dress,
+  win_kitchen,
+  stores,
+  casio_watch,
+  iphone12,
+  brown_jacket,
+  hd_monitor,
+};
